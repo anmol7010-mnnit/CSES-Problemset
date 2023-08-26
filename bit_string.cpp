89f0c5cc71322916694415d6c4d987d03c2c03c3
anmol7010-mnnit <123405290+anmol7010-mnnit@users.noreply.github.com>
2023-08-26 16:55:40 +0530

    Add files via upload

---
+++ bit_string.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+ 
+long long power(long long x, long long y) {
+        
+    if (y == 0)
+        return 1;
+        
+    else if (y % 2 == 0)
+        return power(x, y / 2)%mod * power(x, y / 2)%mod;
+        
+    else
+        return x * power(x, y / 2)%mod * power(x, y / 2)%mod;
+    }
+ 
+int main ()
+{
+    long long n;
+    cin >> n;
+    cout << power(2,n) << endl;
+}
